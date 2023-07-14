0164c6652f36d3e511d62e9dd15722da84cd20d4
Ariel <parnell.ariel@yahoo.com>
2023-07-13 20:08:42 -0500

    added event response and takeoff response

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+
+    let takeOff = document.getElementById("takeoff");
+    let flightStatus = document.getElementById("flightStatus");
+
+
+    takeoff.addEventListener("click", function (event) {
+       let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+
+       if (response) {
+        flightStatus.innerText = "Shuttle in flight.";
+       }
+    });
+
+    
+});
